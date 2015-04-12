e240436f15a5c7c7582fdd43d40d13185365c0df
Christoph Glaubitz <chris@chrigl.de>
2015-04-12 17:51:22 +0200

    settings for packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys, os
+import shlex
+from subprocess import check_output
 
 version = '0.1'
+with open('README.rst') as f:
+    long_description = f.read()
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -16,26 +20,27 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
+GIT_HEAD_REV = check_output(shlex.split('git rev-parse --short HEAD')).strip().decode('utf-8')
+
 setup(name='flattr',
       version=version,
       description="Implementation of a python library for the flattr restful api",
-      long_description="""\
-""",
+      long_description=long_description,
       classifiers=[
           'Development Status :: 4 - Beta',
           'License :: OSI Approved :: Apache Software License',
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Libraries',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
           ],
       keywords='flattr',
       author='Christoph Glaubitz',
       author_email='chris@chrigl.de',
       url='https://github.com/chrigl/python-flattr',
       license='Apache License 2.0',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=['ez_setup', 'examples']),
       include_package_data=True,
       zip_safe=False,
       tests_require=['pytest', 'pytest-cov'],
@@ -49,4 +54,7 @@ setup(name='flattr',
       entry_points="""
       # -*- Entry points: -*-
       """,
+      options = {'egg_info': {
+          'tag_build': '.dev_%s' % GIT_HEAD_REV,
+          }},
       )
